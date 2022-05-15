c8306c963275293edc813e347e8b9e8960a18eec
supperdoggy <supperspggy229@gmail.com>
2022-05-15 18:58:28 +0300

    example ready

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	_ "github.com/supperdoggy/migration_test/migrations"
@@ -16,6 +17,8 @@ func main() {
 	/*
 	   Connect to my cluster
 	*/
+	args := os.Args[1:]
+	log.Println("args", args)
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx)
 	if err != nil {
@@ -23,8 +26,18 @@ func main() {
 	}
 	db := client.Database("test")
 	migrate.SetDatabase(db)
-	if err := migrate.Up(migrate.AllAvailable); err != nil {
-		log.Fatal(err)
+
+	switch args[0] {
+	case "up":
+		if err := migrate.Up(migrate.AllAvailable); err != nil {
+			log.Fatal(err)
+		}
+	case "down":
+		if err := migrate.Down(migrate.AllAvailable); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("you need to pick up or down")
 	}
 
 	log.Println("success")
